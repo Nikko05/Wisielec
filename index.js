8b1bc06a2c461d79cb37fad6cff2aabdd63d302f
niko <nikodemkusiak59@gmail.com>
2022-09-08 10:42:56 +0200

    upload fix with upgrade photo

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const alphabetLetters = ["A", "Ą", "B", "C", "Ć", "D", "E", "Ę", "F", "G", "H
 const button = document.querySelector("button");
 let entry = document.querySelector(".entry").value;
 let entrySecond = "";
+let arrayEntry = [];
 
 const downloadEntry = () => {
     entry = document.querySelector(".entry").value;
     entry = entry.toUpperCase();
 
     for (let i = 0; i < entry.length; i++ ) {
+        arrayEntry[i] = entry[i];
         if (entry[i] == " ") entrySecond = entrySecond + " ";
         else entrySecond = entrySecond + "_";
     }
@@ -44,32 +46,23 @@ let letter = "";
 let image = document.querySelector(".image");
 let filedTried = 0;
 
-const onclickLetter = function(nr) {
+const onclickLetter = function () {
     letter = this;
     letter.classList.remove("active");
     letter.removeEventListener("click", onclickLetter);
-    let amountLetter = 0;
 
     for (let i = 0; i < entry.length; i++) {
-        //console.log(amountLetter++);
         if (letter.className == entry[i]) {
-            let indexOfLetter = entry.indexOf(letter.className);
-            entrySecond = entrySecond.substring(0, indexOfLetter) + entry[i] + entrySecond.substring(indexOfLetter + 1);
+            entrySecond = entrySecond.substring(0, i) + entry[i] + entrySecond.substring(i + 1);
             board.innerHTML = entrySecond;
-            console.log(entry.length);
-            if (entrySecond == entry) {
-                board.innerHTML = `Wygrałeś! Hasło to: ${entry}`;
-                activeLetters.forEach(letter => letter.removeEventListener("click", onclickLetter));
-                return;
-            }
-            else return;
         }
     }
-
-    filedTried++;
-    image.src = `assets/s${filedTried}.jpg`;
-    if (filedTried == 9) {
-        activeLetters.forEach(letter => letter.removeEventListener("click", onclickLetter));
-        board.innerHTML = `Przegrałeś! Hasło to ${entry}`;
+    if (entry.indexOf(letter.className) == -1) {
+        filedTried++;
+        image.src = `assets/s${filedTried}.jpg`;
+        if (filedTried == 9) {
+            activeLetters.forEach(letter => letter.removeEventListener("click", onclickLetter));
+            board.innerHTML = `Przegrałeś! Hasło to ${entry}`;
+        }
     }
 }
